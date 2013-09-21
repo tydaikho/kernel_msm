d3a1827735a5bf82da7304c80f54d6808cb279aa
houst0nn <houstonn@gmail.com>
2013-09-21 21:15:58 +0000

    Makefile: Add optimization & GCC 4.8 fixes
    +return address warning
    +hardcode user@host

--- drivers/usb/gadget/f_qdss.c
+++ drivers/usb/gadget/f_qdss.c
@@ -1,7 +1,7 @@
 /*
  * f_qdss.c -- QDSS function Driver
  *
- * Copyright (c) 2012, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -564,6 +564,7 @@ static int qdss_bind_config(struct usb_configuration *c, const char *name)
 	int status, found = 0;
 	struct usb_qdss_ch *ch;
 	unsigned long flags;
+	int size_of_chname = 0;
 
 	pr_debug("qdss_bind_config\n");
 
@@ -583,7 +584,8 @@ static int qdss_bind_config(struct usb_configuration *c, const char *name)
 
 	spin_lock_irqsave(&d_lock, flags);
 	list_for_each_entry(ch, &usb_qdss_ch_list, list) {
-		if (!strncmp(name, ch->name, sizeof(ch->name))) {
+		size_of_chname = sizeof(ch->name);
+		if (!strncmp(name, ch->name, size_of_chname)) {
 			found = 1;
 			break;
 		}
@@ -726,6 +728,7 @@ struct usb_qdss_ch *usb_qdss_open(const char *name, void *priv,
 	struct f_qdss *qdss;
 	unsigned long flags;
 	int found = 0;
+	int size_of_chname = 0;
 
 	pr_debug("usb_qdss_open\n");
 
@@ -737,7 +740,8 @@ struct usb_qdss_ch *usb_qdss_open(const char *name, void *priv,
 	spin_lock_irqsave(&d_lock, flags);
 	/* Check if we already have a channel with this name */
 	list_for_each_entry(ch, &usb_qdss_ch_list, list) {
-		if (!strncmp(name, ch->name, sizeof(ch->name))) {
+		size_of_chname = sizeof(ch->name);
+		if (!strncmp(name, ch->name, size_of_chname)) {
 			found = 1;
 			break;
 		}
@@ -814,4 +818,3 @@ static int qdss_setup(void)
 {
 	return 0;
 }
-
