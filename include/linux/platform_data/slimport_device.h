4784c7c862446a9564e9531cba8c47ecadb0586b
houst0nn <houstonn@gmail.com>
2013-09-29 01:11:30 +0000

    remove Display: slimport V0.3

--- include/linux/platform_data/slimport_device.h
+++ include/linux/platform_data/slimport_device.h
@@ -24,12 +24,8 @@ struct anx7808_platform_data
 
 	spinlock_t lock;
 
-	int (*switch_power)(bool on);
-
-	unsigned int phy_reg2;
-	unsigned int phy_reg12;
-	unsigned int phy_reg6;
-	unsigned int phy_reg16;
+	int (*dvdd_power)(bool on);
+	int (*avdd_power)(bool on);
 };
 
 #endif
